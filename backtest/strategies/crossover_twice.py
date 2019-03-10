41b006ed889f1ce27fc5fc6f3645b3f11db809a4
Kwesi Parry Apponsah <kwesi@oddjob.utias.utoronto.ca>
2019-03-10 14:33:30 -0400

    test moving average crossover strategy

--- backtest/strategies/crossover_twice.py
+++ backtest/strategies/crossover_twice.py
@@ -26,25 +26,25 @@ def handle_data(context, data):
 
     # skip first 60 days to get full windwos
     context.i += 1
-    if context.i < 100:
+    if context.i < 30:
         return
 
     # compute avarages and check lows and highs
     for stock in context.stocks:
-        long_mavg  = data.history(stock,'price',100,"1d").mean()
+        long_mavg  = data.history(stock,'price',30,"1d").mean()
         short_mavg = data.history(stock,'price',10,"1d").mean()
 
-        high_long  = data.history(stock,'high',10,"1d").max()
-        high_short = data.history(stock,'high',5,"1d").min()
+        #high_long  = data.history(stock,'high',10,"1d").max()
+        #high_short = data.history(stock,'high',5,"1d").min()
 
-        low_long  = data.history(stock,'low',10,"1d").max()
-        low_short = data.history(stock,'low',5,"1d").min()
+        #low_long  = data.history(stock,'low',10,"1d").max()
+        #low_short = data.history(stock,'low',5,"1d").min()
 
         buy_signal = short_mavg > long_mavg
-        buy_signal = buy_signal & (low_short > low_long)
+        #buy_signal = buy_signal & (low_short > low_long)
 
         sell_signal = short_mavg < long_mavg
-        sell_signal = sell_signal & (high_short < high_long)
+        #sell_signal = sell_signal & (high_short < high_long)
 
         if buy_signal:
             order_target_percent(stock,target[stock.symbol])
@@ -52,8 +52,9 @@ def handle_data(context, data):
         if sell_signal:
             order_target_percent(stock,0.)
 
-        #record(
-        #    stock.price=data.current(stock,"price"),
-        #    short_mavg=short_mavg,
-        #    long_mavg=long_mavg
-        #    )
+        kwargs = {
+            stock.symbol:data.current(stock,"price"),
+            "short_mavg":short_mavg,
+            "long_mavg":long_mavg
+        }
+        record(**kwargs)
